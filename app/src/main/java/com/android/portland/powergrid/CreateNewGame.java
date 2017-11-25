09159a9310c3ac1c57f48a070c9e879933ec790e
Daniel Shaw <dpshaw6@gmail.com>
2017-11-25 15:25:21 -0800

    Empty layout for creating a new game.

---
+++ app/src/main/java/com/android/portland/powergrid/CreateNewGame.java
@@ -0,0 +1,18 @@
+package com.android.portland.powergrid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dpsha on 23-Nov-17.
+ */
+
+public class CreateNewGame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_createnewgame);
+    }
+
+}
--- app/src/main/java/com/android/portland/powergrid/TitleActivity.java
+++ app/src/main/java/com/android/portland/powergrid/TitleActivity.java
@@ -1,7 +1,9 @@
 package com.android.portland.powergrid;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TitleActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_title);
+    }
+
+    public void createNewGame(View view) {
+        Intent intent = new Intent(this, CreateNewGame.class);
+        startActivity(intent);
     }
 }
